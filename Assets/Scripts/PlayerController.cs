0dc032d5291bc5eb5b9c2b499eabc8a75e30cbd7
aidan <ferentinosaidan@gmail.com>
2024-05-27 18:03:13 +0200

    anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,24 +5,31 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 1;
     [SerializeField] private new Rigidbody2D rigidbody;
+    [SerializeField] private Animator animator;
+    private Vector2 movement;
 
     void Start()
     {
         rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         MovePlayer();
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void MovePlayer()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        Vector3 movementVector = new Vector3(horizontal, vertical, 0);
+        Vector3 movementVector = new Vector3(movement.x, movement.y, 0);
         movementVector = movementVector.normalized * movementSpeed * Time.deltaTime;
 
         rigidbody.MovePosition(rigidbody.transform.position + movementVector);
